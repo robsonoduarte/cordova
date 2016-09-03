73bb863d2abd0d2388bff3ae1116219e1eecafe7
Robson Duarte <robson.o.d@gmail.com>
2016-09-03 06:59:12 -0300

    continue writing the readme.

--- scala-push-notification/README.md
+++ scala-push-notification/README.md
@@ -1,5 +1,12 @@
-Scala Push Notification
+Push Notification using Scala
 ---------------------------------
 In this simple example we use the [OneSignal](https://onesignal.com/) Cross Platform Push Notification Delivery.
 
-*readme in construction*
+For receive the Notification we used the hybrid mobile with [Apache Cordova](http://cordova.apache.org/) [here](https://github.com/robsonoduarte/learn-cordova/tree/master/push-notification) is the exampel we used.
+
+
+*README IN CONSTRUCTION*
+
+
+
+
--- scala-push-notification/src/main/scala/br/com/mystudies/cordova/push/notificaton/PushNotification.scala
+++ scala-push-notification/src/main/scala/br/com/mystudies/cordova/push/notificaton/PushNotification.scala
@@ -6,14 +6,14 @@ object PushNotification extends App {
 
   val json = """{"app_id": "your_id","included_segments": ["All"],"data": {"foo": "bar"},"contents": {"en": "English Message"}}"""
 
-  val response = Http("https://onesignal.com/api/v1/notifications")
+  val resp = Http("https://onesignal.com/api/v1/notifications")
     .postData(json)
     .header("content-type", "application/json; charset=UTF-8")
     .header("Authorization", "Basic your_key")
     .asString
 
 
-  println(response.body)
+  println(resp.body)
 
 }
 
