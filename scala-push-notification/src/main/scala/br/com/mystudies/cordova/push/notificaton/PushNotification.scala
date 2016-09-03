d1774afe7f568106f409068c1d9229b1ee810485
Robson Duarte <robson.o.d@gmail.com>
2016-09-02 22:03:57 -0300

    remove device id and key api.

--- scala-push-notification/src/main/scala/br/com/mystudies/cordova/push/notificaton/PushNotification.scala
+++ scala-push-notification/src/main/scala/br/com/mystudies/cordova/push/notificaton/PushNotification.scala
@@ -4,12 +4,12 @@ import scalaj.http.Http
 
 object PushNotification extends App {
 
-  var json = """{"app_id": "b5e7a9a4-35f2-4abc-a5c8-5c8125c386f0","included_segments": ["All"],"data": {"foo": "bar"},"contents": {"en": "English Message"}}"""
+  var json = """{"app_id": "your_id","included_segments": ["All"],"data": {"foo": "bar"},"contents": {"en": "English Message"}}"""
 
   val response = Http("https://onesignal.com/api/v1/notifications")
     .postData(json)
     .header("content-type", "application/json; charset=UTF-8")
-    .header("Authorization", "Basic ZTBmOWIxNDYtNmNlZi00YTE4LWFkZDEtYzFlNGM5YWMwZmNh")
+    .header("Authorization", "Basic your_key")
     .asString
 
   println(response.code)
