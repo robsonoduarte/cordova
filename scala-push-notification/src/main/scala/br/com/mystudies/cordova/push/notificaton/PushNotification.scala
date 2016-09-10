20c4e67c906d9732e232653a982cc2531fd65938
Robson Duarte <robson.o.d@gmail.com>
2016-09-09 21:41:52 -0300

    add val to setting of oneSingal app.

--- scala-push-notification/src/main/scala/br/com/mystudies/cordova/push/notificaton/PushNotification.scala
+++ scala-push-notification/src/main/scala/br/com/mystudies/cordova/push/notificaton/PushNotification.scala
@@ -4,14 +4,16 @@ import scalaj.http.Http
 
 object PushNotification extends App {
 
-  val json = """{"app_id": "your_id","included_segments": ["All"],"data": {"foo": "bar"},"contents": {"en": "English Message"}}"""
+  val app_id = "" // <-- OneSignal App ID
+  val api_key = "" // <-- REST API Key
+
+  val json = s"""{"app_id": "$app_id","included_segments": ["All"],"data": {"foo": "bar"},"contents": {"en": "English Message"}}"""
 
   val resp = Http("https://onesignal.com/api/v1/notifications")
     .postData(json)
     .header("content-type", "application/json; charset=UTF-8")
-    .header("Authorization", "Basic your_key")
+    .header("Authorization", s"Basic $api_key")
     .asString
-
 
   println(resp.body)
 
